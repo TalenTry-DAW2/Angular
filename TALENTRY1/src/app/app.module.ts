71e0399219a56875582091d75be3affe573bf076
APEREZ2002 <119208468+APEREZ2002@users.noreply.github.com>
2024-02-14 16:27:19 +0100

    Componentes creados
    Componentes basicos creados

--- TALENTRY1/src/app/app.module.ts
+++ TALENTRY1/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    LoginComponent,
+    RegistroComponent,
+    PaginaPrincipalComponent
   ],
   imports: [
     BrowserModule,
