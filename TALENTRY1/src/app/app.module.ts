ee3c1a29478c48eaed4a4913a8b6d078d8d30e6a
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2024-03-19 16:26:43 +0100

    login funcional
    - servicio de auth hecho,
    servicio de token hecho

--- TALENTRY1/src/app/app.module.ts
+++ TALENTRY1/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { ResultadosEntrevistaComponent } from './resultados-entrevista/resultados-entrevista.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -48,8 +49,8 @@ import { ResultadosEntrevistaComponent } from './resultados-entrevista/resultado
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-
-    FormsModule
+    HttpClientModule,
+    FormsModule,
 
   ],
   providers: [],
