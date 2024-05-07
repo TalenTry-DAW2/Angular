6df23688692f180d5fa811c46382395fba739676
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2024-05-07 17:26:55 +0200

    ver preguntas empresa

--- TALENTRY1/src/app/servicios/empresa.service.ts
+++ TALENTRY1/src/app/servicios/empresa.service.ts
@@ -72,4 +72,18 @@ export class EmpresaService {
     //peticion con headers de actualizacion
     return this.http.get("http://127.0.0.1:8000/api/user/totalPorcentajeEntrevista", { headers });
   }
+
+  getPreguntas(id:number): Observable<any> {
+    // token de sesion
+    const authToken = this.tokenService.getToken();
+    // header con el token
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${authToken}`,
+    });
+    let params = new HttpParams()
+      .set('id', id);
+    //peticion con headers de actualizacion
+    return this.http.get(`http://127.0.0.1:8000/api/question/getFromCategoryAll`, { params, headers });
+  }
 }
+
