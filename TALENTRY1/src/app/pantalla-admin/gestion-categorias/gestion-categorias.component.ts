20684e8d4be309342391d48baf4027676b9df029
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2024-05-16 17:38:21 +0200

    categorias visibles

--- TALENTRY1/src/app/pantalla-admin/gestion-categorias/gestion-categorias.component.ts
+++ TALENTRY1/src/app/pantalla-admin/gestion-categorias/gestion-categorias.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EmpresaService } from 'src/app/servicios/empresa.service';
+import { category } from 'src/models/category';
 
 @Component({
   selector: 'app-gestion-categorias',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestion-categorias.component.css']
 })
 export class GestionCategoriasComponent implements OnInit {
-
-  constructor() { }
+  categorias: category[] = [];
+  constructor(private empService: EmpresaService) { }
 
   ngOnInit(): void {
+    this.empService.getCategory().subscribe(data => {
+      this.categorias = data[0];
+    });
   }
 
 }
